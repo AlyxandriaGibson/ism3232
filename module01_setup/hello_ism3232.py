fe94734aa68cc10a20cab7fd4f1420954dc232ce
Alyxandria Gibson <alyxandria.ang@gmail.com>
2026-09-01 21:56:42 -0400

    add module01 and module02 lab folders

---
+++ module01_setup/hello_ism3232.py
@@ -0,0 +1,9 @@
+# hello_ism3232.py
+# Author: Alyxandria Gibson
+print('Hello, ISM3232!')
+course_name = 'Business Application Development'
+credit_hours = 3
+weekly_hours = credit_hours * 2
+print(f'Course: {course_name}')
+print(f'Expected weekly hours: {weekly_hours}')
+print('Environment verified. Week 1 complete.')
